6d6153c1272820ed5f7d2a23901ae009c8ccff6d
Emona Santiago <emonasantiago1207@gmail.com>
2023-03-02 00:50:11 +0100

    Footer start

---
+++ src/components/footer.svelte
@@ -0,0 +1,40 @@
+<section id="footer">
+  <h2>FDND microsites</h2>
+  <p>
+    FDND maakt gebruik van microsites. Microsites zijn kleine websites ter
+    ondersteuning van een campagne, informatie over een product, dienst of voor
+    een ander specifiek doel.
+  </p>
+
+  <div class="links">
+    <p><b><a href="default.asp" target="_blank">FDND.nl</a></b></p>
+    <p>
+      <b
+        ><a href="default.asp" target="_blank">Contact en Routebeschrijving</a
+        ></b
+      >
+    </p>
+    <p><b><a href="default.asp" target="_blank">Programma</a></b></p>
+    <p><b><a href="default.asp" target="_blank">Partners </a></b></p>
+  </div>
+</section>
+
+<style>
+  h2 {
+    color: white;
+  }
+
+  .links {
+    background-color: #a675f5;
+    border-radius: 2em;
+    padding: 1em;
+  }
+
+  p {
+    color: white;
+  }
+
+  a {
+    color: white;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,14 @@
   import Sliders from "../components/sliders.svelte";
   import Upcoming from "../components/upcoming.svelte";
   import SignIn from "../components/sign-in.svelte";
+  import Footer from "../components/footer.svelte";
 </script>
 
 <Header />
 <Sliders />
 <Upcoming />
 <SignIn />
+<Footer />
 
 <style>
   * {
