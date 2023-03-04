c8cbbf094e8e8c40a053ecf4660cbf9c296df980
Emona Santiago <emonasantiago1207@gmail.com>
2023-03-05 00:24:20 +0100

    hero - section basis

---
+++ src/components/1header.svelte
@@ -0,0 +1,71 @@
+<head>
+  <title>My awesome website</title>
+</head>
+
+<body>
+  <section class="hero">
+    <div class="hero-inner">
+      <h1>My awesome website</h1>
+      <h2>Look at this website and bask in its amazing glory!</h2>
+      <a href="https://example.com/" class="btn">Go ahead...</a>
+    </div>
+  </section>
+</body>
+
+<style>
+  body {
+    margin: 0;
+  }
+  .hero {
+    /* Sizing */
+    width: auto;
+    height: 100vh;
+
+    /* Flexbox stuff */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    /* Text styles */
+    text-align: center;
+    color: white; /* ADD THIS LINE */
+
+    /* Background styles */
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+      url("https://codetheweb.blog/assets/img/posts/full-image-hero/image.jpg");
+    background-size: cover;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+  }
+
+  .hero h1 {
+    /* Text styles */
+    font-size: 5em;
+
+    /* Margins */
+    margin-top: 0;
+    margin-bottom: 0.5em;
+  }
+
+  .hero .btn {
+    /* Positioning and sizing */
+    display: block;
+    width: 200px;
+
+    /* Padding and margins */
+    padding: 1em;
+    margin-top: 50px;
+    margin-left: auto;
+    margin-right: auto;
+
+    /* Text styles */
+    color: white; /* CHANGE THIS LINE */
+    text-decoration: none;
+    font-size: 1.5em;
+
+    /* Border styles */
+    border: 3px solid white; /* CHANGE THIS LINE */
+    border-radius: 20px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
-  import Header from "../components/header.svelte";
+  // import Header from "../components/header.svelte";
+  import Header from "../components/1header.svelte";
   import Sliders from "../components/sliders.svelte";
   import Upcoming from "../components/upcoming.svelte";
   import SignIn from "../components/sign-in.svelte";
   import Footer from "../components/footer.svelte";
 </script>
 
+<!-- <Header /> -->
 <Header />
 <Sliders />
 <Upcoming />
