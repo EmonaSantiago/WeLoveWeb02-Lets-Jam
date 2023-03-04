bf18376b312dfb0ff63ac91d73f637c66ad05a08
Emona Santiago <emonasantiago1207@gmail.com>
2023-03-04 14:39:09 +0100

    Sign-in fix

--- src/components/sign-in.svelte
+++ src/components/sign-in.svelte
@@ -1,4 +1,5 @@
 <div>
+  <h2>Meld je hier aan!</h2>
   <form action="/action_page.php">
     <label for="fname">First Name</label>
     <input type="text" id="fname" name="firstname" placeholder="Your name.." />
@@ -11,11 +12,11 @@
       placeholder="Your last name.."
     />
 
-    <label for="country">Country</label>
+    <label for="country">We 🤍 Web sessie</label>
     <select id="country" name="country">
-      <option value="australia">Australia</option>
-      <option value="canada">Canada</option>
-      <option value="usa">USA</option>
+      <option value="">CSS</option>
+      <option value="">Html</option>
+      <option value="">Svelte</option>
     </select>
 
     <input type="submit" value="Submit" />
@@ -23,20 +24,26 @@
 </div>
 
 <style>
+  h2 {
+    color: #050542;
+    text-align: center;
+  }
   input[type="text"],
   select {
     width: 100%;
     padding: 12px 20px;
     margin: 8px 0;
     display: inline-block;
-    border: 1px solid #050542;
-    border-radius: 4px;
+    border-radius: 0.5rem;
+    border-width: 1px;
+    border-style: solid;
+    border-color: #050542;
     box-sizing: border-box;
   }
 
   input[type="submit"] {
     width: 100%;
-    background-color: #a675f5;
+    background-color: #050542;
     color: white;
     padding: 14px 20px;
     margin: 8px 0;
@@ -46,13 +53,13 @@
   }
 
   input[type="submit"]:hover {
-    background-color: #45a049;
+    background-color: #a675f5;
   }
 
   div {
-    border-radius: 2em;
+    border-radius: 0.5rem;
     background-color: #66e5bf;
-    padding: 20px;
-    margin: 1em;
+    margin-bottom: 2rem;
+    padding: 2rem;
   }
 </style>
